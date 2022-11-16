d1fcc279eba847ca6f6d5d9c040c5e0a9292cf5f
Cargomillieux <116322048+Cargomillieux@users.noreply.github.com>
2022-11-16 14:41:28 -0500

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+# Vert = P0
+# Jaune = P1
+# Rouge = P2
+# LED Jaune = P8
+# LED Blanc = P16
 def on_forever():
     pins.digital_write_pin(DigitalPin.P0, 1)
-    
-    while input.button_is_pressed(Button.A):
-        pins.digital_write_pin(DigitalPin.P2, 01
-        
+    pins.digital_write_pin(DigitalPin.P1, 0)
+    if input.button_is_pressed(Button.A):
+        pins.digital_write_pin(DigitalPin.P0, 1)
+
+        while basic.pause(500):
+            pins.digital_write_pin(DigitalPin.P1, 1)
+            pins.digital_write_pin(DigitalPin.P0, 0)
+        while basic.pause(500):
+            pins.digital_write_pin(DigitalPin.P1, 0)
+
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
+//  Vert = P0
+//  Jaune = P1
+//  Rouge = P2
+//  LED Jaune = P8
+//  LED Blanc = P16
 basic.forever(function on_forever() {
     pins.digitalWritePin(DigitalPin.P0, 1)
-    while (input.buttonIsPressed(Button.A)) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    if (input.buttonIsPressed(Button.A)) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        while (basic.pause(500)) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        }
+        while (basic.pause(500)) {
+            pins.digitalWritePin(DigitalPin.P1, 0)
+        }
     }
+    
 })
