18b17102a3422cdc6903e09665b26557cf7d569f
Cargomillieux <116322048+Cargomillieux@users.noreply.github.com>
2022-11-21 16:11:27 -0500

    Mise à jour main.ts, main.py

--- main.py
+++ main.py
@@ -3,16 +3,74 @@
 # Rouge = P2
 # LED Jaune = P8
 # LED Blanc = P16
-def on_forever():
+
+#cycle pieton
+def cycle_pieton():
+    pins.digital_write_pin(DigitalPin.P0, 1)#v
+    pins.digital_write_pin(DigitalPin.P2, 0)#r
+    basic.pause(1000)
+    pins.digital_write_pin(DigitalPin.P1, 1)#j
+    pins.digital_write_pin(DigitalPin.P0, 0)#v
+    basic.pause(1000)
+    pins.digital_write_pin(DigitalPin.P1, 0)#j
+    pins.digital_write_pin(DigitalPin.P2, 1)#r
+    pins.digital_write_pin(DigitalPin.P0, 0)#v
+    pins.digital_write_pin(DigitalPin.P16, 1)#b
+    basic.pause(3000)
+    pins.digital_write_pin(DigitalPin.P16, 0)#b
+    pins.digital_write_pin(DigitalPin.P8, 1)#jl
+    basic.pause(1000)
+    pins.digital_write_pin(DigitalPin.P8, 0)#jl
+    pins.digital_write_pin(DigitalPin.P2, 0)#r
+
+
+#cycle normale 
+def cycle():
     pins.digital_write_pin(DigitalPin.P0, 1)
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
     pins.digital_write_pin(DigitalPin.P1, 0)
     if input.button_is_pressed(Button.A):
-        pins.digital_write_pin(DigitalPin.P0, 1)
+            cycle_pieton()
+    pins.digital_write_pin(DigitalPin.P2, 0)
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    basic.pause(1000)
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    pins.digital_write_pin(DigitalPin.P0, 0)#v
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    pins.digital_write_pin(DigitalPin.P1, 1)#J
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    pins.digital_write_pin(DigitalPin.P2, 0)#r
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    basic.pause(1000)
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    pins.digital_write_pin(DigitalPin.P2, 1)#r
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    pins.digital_write_pin(DigitalPin.P1, 0)#j
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    pins.digital_write_pin(DigitalPin.P0, 0)#v
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+    basic.pause(1000)
+    if input.button_is_pressed(Button.A):
+            cycle_pieton()
+        
+    
+def on_forever():
+    cycle()
+    if input.button_is_pressed(Button.A):
+        cycle_pieton()
+    
 
-        while basic.pause(500):
-            pins.digital_write_pin(DigitalPin.P1, 1)
-            pins.digital_write_pin(DigitalPin.P0, 0)
-        while basic.pause(500):
-            pins.digital_write_pin(DigitalPin.P1, 0)
+    
+
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -3,18 +3,112 @@
 //  Rouge = P2
 //  LED Jaune = P8
 //  LED Blanc = P16
-basic.forever(function on_forever() {
+// cycle pieton
+function cycle_pieton() {
     pins.digitalWritePin(DigitalPin.P0, 1)
+    // v
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    // r
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    // j
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    // v
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    // j
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    // r
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    // v
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    // b
+    basic.pause(3000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    // b
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    // jl
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    // jl
+    pins.digitalWritePin(DigitalPin.P2, 0)
+}
+
+// r
+// cycle normale 
+function cycle() {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
     pins.digitalWritePin(DigitalPin.P1, 0)
     if (input.buttonIsPressed(Button.A)) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        while (basic.pause(500)) {
-            pins.digitalWritePin(DigitalPin.P1, 1)
-            pins.digitalWritePin(DigitalPin.P0, 0)
-        }
-        while (basic.pause(500)) {
-            pins.digitalWritePin(DigitalPin.P1, 0)
-        }
+        cycle_pieton()
+    }
+    
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    basic.pause(1000)
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    // v
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    // J
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    // r
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    basic.pause(1000)
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    // r
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    // j
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    // v
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+    basic.pause(1000)
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
+    }
+    
+}
+
+basic.forever(function on_forever() {
+    cycle()
+    if (input.buttonIsPressed(Button.A)) {
+        cycle_pieton()
     }
     
 })
